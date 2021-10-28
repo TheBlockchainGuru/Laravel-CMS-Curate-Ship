30e29c24a7bf6b459d840def046155ee5cb186fc
timoflask <88914566+timoflask@users.noreply.github.com>
2021-10-28 20:15:05 +0300

    first commit

---
+++ Modules/Admin/Resources/views/editor/component/partials/preview-modal.blade.php
@@ -0,0 +1,31 @@
+<div class="modal modal--animate-scale flex flex-center bg-black bg-opacity-90% padding-md js-modal" id="preview-modal">
+  <div class="modal__content width-100% max-width-lg max-height-100% overflow-auto bg radius-md inner-glow shadow-md" role="alertdialog" aria-labelledby="modal-title-1" aria-describedby="modal-description-1">
+
+
+      <button class="reset modal__close-btn modal__close-btn--inner hide@md js-modal__close js-tab-focus">
+        <svg class="icon" viewBox="0 0 20 20">
+          <title>Close modal window</title>
+          <g fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <line x1="3" y1="3" x2="17" y2="17" />
+            <line x1="17" y1="3" x2="3" y2="17" />
+          </g>
+        </svg>
+      </button>
+
+
+    <div class="padding-y-sm padding-x-md">
+      <div class="text-component" id="previewPanel">
+        
+      </div>
+    </div>
+  </div>
+  <button class="reset modal__close-btn modal__close-btn--outer display@md js-modal__close js-tab-focus">
+    <svg class="icon icon--sm" viewBox="0 0 24 24">
+      <title>Close modal window</title>
+      <g fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <line x1="3" y1="3" x2="21" y2="21" />
+        <line x1="21" y1="3" x2="3" y2="21" />
+      </g>
+    </svg>
+  </button>
+</div>
